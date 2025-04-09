f3ea68d2ae31e440f03b1e95ff3bced795b610f7
emmanuelowusu1241 <jeffkprint@gmail.com>
2025-04-09 00:18:52 -0700

    Update Solutions.py
    solution to assignment 3

--- Assignment 3/Solutions.py
+++ Assignment 3/Solutions.py
@@ -1,4 +1,3 @@
-"""
 #Solutions_5240100507_2025.py
 # 1 Reverse the string "Programming"
 text = "Programming"
@@ -25,21 +24,3 @@ text = "This is a string and it is an example."
 print("Modified string:", text.replace("is", "was"))
 
 
-"""
-3.Write a Python program to check if a given string is a palindrome. A palindrome reads the same forwards
-and backward (e.g., "radar", "level"). Hint: Compare the string with its reverse.
-"""
-
-
-
-"""
-4.Create a Python program that asks the user to enter a sentence and counts the number of words in the sentence.
-Hint: Use the split() method to break the string into words.
-"""
-
-
-
-"""
-5.Write a Python program to replace all occurrences of "is" with "was" in the string "This is a string and it
-is an example." Print the modified string.
-"""
